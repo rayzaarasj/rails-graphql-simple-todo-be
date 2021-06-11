7d9968e4baa6b539ff949ee0d2ba7f3eaf226d52
Rayza Arasj Mahardhika <rayza.arasj@gmail.com>
2021-06-11 13:09:28 +0700

    add delete category mutation

---
+++ app/graphql/mutations/delete_category.rb
@@ -0,0 +1,22 @@
+class Mutations::DeleteCategory < Mutations::BaseMutation
+  argument :id, Integer, required: true
+
+  field :deletedId, Integer, null: false
+  field :errors, [String], null: false
+
+  def resolve(id:)
+    category = Category.find(id)
+
+    if category.destroy
+      {
+        deletedId: id,
+        errors: nil
+      }
+    else
+      {
+        deletedId: nil,
+        errors: category.errors.full_messages
+      }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,6 +5,7 @@ module Types
     field :delete_todo, mutation: Mutations::DeleteTodo
     field :create_category, mutation: Mutations::CreateCategory
     field :update_category, mutation: Mutations::UpdateCategory
+    field :delete_category, mutation: Mutations::DeleteCategory
 
     # TODO: remove me
     field :test_field, String, null: false,
