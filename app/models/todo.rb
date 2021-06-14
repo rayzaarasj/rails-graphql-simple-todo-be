7e6660a537383aed5d46685d8309a0d43b054ff7
Rayza Arasj Mahardhika <rayza.arasj@gmail.com>
2021-06-14 10:49:56 +0700

    simple model test todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,6 @@
 class Todo < ApplicationRecord
+    validates_presence_of :title
+    validates_presence_of :description
+    validates_presence_of :deadline
     has_and_belongs_to_many :categories
 end
---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Todo, :type => :model do
+
+  subject {
+    described_class.new(
+      title: "test_title",
+      description: "test_description",
+      deadline: DateTime.now + 1.day
+    )
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a deadline' do
+    subject.deadline = nil
+    expect(subject).to_not be_valid
+  end
+end
